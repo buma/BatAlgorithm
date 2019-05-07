51b8579e0aa985d5649a73280658befeffb7690d
firefly-cpp <pingvin1@gmail.com>
2019-05-07 10:28:02 +0200

    News setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(name='BatAlgorithm',
+      description='Bat algorithm implementation',
+      author='Iztok Fister Jr. and Marko Burjek',
+      version='0.2',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+      ],
+      url='https://github.com/firefly-cpp/BatAlgorithm',
+      py_modules=['BatAlgorithm']
+      ) 
+
