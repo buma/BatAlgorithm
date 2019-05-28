a7792901c5a25b61f042fe9796a4e101b37fa14d
firefly-cpp <pingvin1@gmail.com>
2019-05-28 18:36:28 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] < 3:
+    with open('README.md') as f:
+        long_description = f.read()
+else:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+
 setup(name='BatAlgorithm',
       description='Bat algorithm implementation',
+      long_description_content_type="text/markdown",	
+      long_description=long_description,
       author='Iztok Fister Jr. and Marko Burjek',
-      version='0.2',
+      version='0.3.1',
       license='MIT',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -19,7 +31,8 @@ setup(name='BatAlgorithm',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development'
       ],
-      url='https://github.com/firefly-cpp/BatAlgorithm',
+      include_package_data=True,	
+      url='https://github.com/buma/BatAlgorithm',
       py_modules=['BatAlgorithm']
       ) 
 
